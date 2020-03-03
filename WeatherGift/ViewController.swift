1a2d8726013fed72420c44caa26c4f461a5f09d5
Yidan Ma <madt@bc.edu>
2020-03-03 16:17:09 -0500

    Initial Commit

---
+++ WeatherGift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherGift
+//
+//  Created by mayidan on 3/3/20.
+//  Copyright © 2020 mayidan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
