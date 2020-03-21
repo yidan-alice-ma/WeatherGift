14ddd0a25433dddf0e3bf84ee2f85801092ba831
Yidan Ma <madt@bc.edu>
2020-03-21 16:24:20 -0400

    7.4 - with .gitignore

---
+++ WeatherGift/APIkeys.swift
@@ -0,0 +1,14 @@
+//
+//  APIkeys.swift
+//  WeatherGift
+//
+//  Created by mayidan on 3/21/20.
+//  Copyright © 2020 mayidan. All rights reserved.
+//
+
+import Foundation
+
+struct APIkeys {
+    static let googlePlacesKey = "AIzaSyBc3V2GgkLOklvzUfC9wiKGfjiwhy5ftcs"
+}
+
